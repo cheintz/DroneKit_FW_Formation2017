Fix velocity sign in pdiDot

flightControl/control.py
@@ -780,7 +780,7 @@ class Controller(threading.Thread): 	#Note: This is a thread, not a process,  be
 		chiiDot = RgDot * di #chiiDot is not filtered
 		#CS.rotFFTerm[2] = lowPassFilter(chiiDot[2], CS.rotFFTerm[2], GAINS['aFiltAccelVert'],CS.rotFFTerm[2] == 0.0)
 		CS.rotFFTerm[2] = lowPassFilter(chiiDot[2], CS.rotFFTerm[2], GAINS['aFiltAccelVert']) #Start at zero to reject large initial condition
-		CS.rotFFTerm[0:2] = chiiDot[0:2]
+		CS.rotFFTerm[0:2] = chiiDot[0:2] #Only sets the first 2 elements. Silly python indexing is silly.
 
 		if(THIS.parameters.config['LeaderAccelSource'] == 'Accel'):
 			pass #use pgDot as is
@@ -837,7 +837,7 @@ class Controller(threading.Thread): 	#Note: This is a thread, not a process,  be
 		pdi= CS.pgTerm + CS.rotFFTerm + GAINS['ki'] * mu(xiiNormSquared) * xii
 		rhoPrime = mu(xiiNormSquared) * np.eye(3) + 2 * muPrime(xiiNormSquared) * xii * xii.T
 
-		pdiDot = 1*pgDot+1*RgDDot*di - GAINS['ki'] * rhoPrime * xiiDot
+		pdiDot = 1*pgDot+1*RgDDot*di + GAINS['ki'] * rhoPrime * xiiDot
 
 		self.pm.p('pdiDot:' + str(pdiDot))
 

flightControl/defaultConfig.py
@@ -24,7 +24,7 @@ def getParams():
 	# 										 [[-10, 0, 0], [7, 7, 0],[7, -7, 0]]]) #for QP test
 	#aFiltAccel to 1 for no filtering
 
-	defaultParams.gains = {'kl':1.0/7.0*np.diag([1,1,0.3]) , 'ka': 0.3/7.0*np.diag([1,1,0.3])
+	defaultParams.gains = {'kl':1.0/7.0*np.diag([1,1,0.3]) , 'ka': 1.0/7.0*np.diag([1,1,0.3])
 		,'vMin': 16,'vMax':35,'aFilterHdg':0.4,'aFiltAccelVert':0.02482,'aFiltAccelHoriz':0.3941
 		,'kAlt':KPID(.026, .0017,.0105), 'asi':2.0,'bsi':0.4,'asigmai':1.0,'bsigmai':0.2,'agammai':0.6,'bgammai':0.1 #a for speed, b for heading, c for pitch
 		,'deltasi': 0.1, 'deltasigmai': 0.5, 'deltagammai': 0.1, 'barxsi': 6.0, 'barxsigmai': 1, 'barxgammai': 1.5 #Switch widths and integrator saturations